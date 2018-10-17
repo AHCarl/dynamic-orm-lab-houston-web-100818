f13d192e22199f70adbbebc237e98e8cd635f484
Alexander Carlin <ahcarlin@gmail.com>
2018-10-17 21:23:45 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -49,8 +49,8 @@ class InteractiveRecord
   end
   
   def self.find_by(arg:)
-    sql =<<-SQL
-    SELECT * FROM "#{self.table_name}" WHERE 
+    sql ="SELECT * FROM '#{self.table_name}' WHERE '#{arg:}' = '#{arg}'"
+    DB[:conn].execute(sql)
   end
   
 end
